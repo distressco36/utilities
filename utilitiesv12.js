d62a858ad895abac4fd48992ad7c43f7e114760a
distressco36 <93086667+distressco36@users.noreply.github.com>
2022-01-25 04:20:49 -0800

    Update and rename utilitiesv11.js to utilitiesv12.js

--- utilitiesv12.js
+++ utilitiesv12.js
@@ -251,9 +251,10 @@ function removePostPlane(inputgrid, postbtn){
 }
 	
 function sendToDB(upfile, uptitle, uptext){
-	var channel = currentTopic.trim().replaceAll(' ', '+');
+	var channel = currentTopic.trim(); //.replaceAll(' ', '+');
+	var channelShareForm = currentTopic.trim().replaceAll(' ', '+');
 	var postid = Math.random().toString(20).substr(2, 11).toUpperCase();
-	var shareURL = `https://distressco.com/content/?ch=${channel}&postid=${postid}`;
+	var shareURL = `https://distressco.com/content/?ch=${channelShareForm}&postid=${postid}`;
 	if(!upfile){
 		var uptextp = processInputTxt(uptext);
 		var dataToSend = {channel: channel, postid: postid, shareurl: shareURL, fileobject: '', caption: '',
