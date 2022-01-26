78d5aa2fc93b2b8464e7807d14ec7afd64234e8e
distressco36 <93086667+distressco36@users.noreply.github.com>
2022-01-26 14:36:11 -0800

    Update and rename utilities21.js to utilities.js

--- utilities.js
+++ utilities.js
@@ -357,9 +357,8 @@ function buildMessage(meta){
     var sitename = (meta['og:site_name']) ? meta['og:site_name'] : '';
 	var title = (meta['og:title']) ? meta['og:title'] : (meta['twitter:title']) ? meta['twitter:title'] : meta['fallback:title'] ? meta['fallback:title'] : '';
 	var description = (meta['og:description']) ? meta['og:description'] : (meta['twitter:description']) ? meta['twitter:description'] : (meta['description']) ? meta['description'] : '';
-	console.log(image);
 	var imagesecure = '';
-	if(image.includes('http')){imagesecure = image.replace('http', 'https'); console.log(imagesecure);}
+	if(image.includes('http')){imagesecure = image.replace('http', 'https');}
     var message = `<div class='post-message-url'><a href=${url.toString()} target='_blank'>${url.toString()}</a></div>
 <div class='post-preview-box'>
 <img src=${imagesecure} class='post-preview-img'></img>
